925b09d6cf3499242f0764c0f5fa235647fe3d83
Marija <2232817m@student.gla.ac.uk>
2018-03-12 00:16:00 +0000

    Commented out functions in admin.py & models.py

--- tatu/admin.py
+++ tatu/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from tatu.models import Category, Page
+#from tatu.models import Category, Page
 
 
 # Register your models here.
-admin.site.register(Category)
+#admin.site.register(Category)
 #admin.site.register(Page)
 
 
-class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'url')
+#class PageAdmin(admin.ModelAdmin):
+#    list_display = ('title', 'category', 'url')
 
-admin.site.register(Page, PageAdmin)
+#admin.site.register(Page, PageAdmin)
