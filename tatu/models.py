ebd8abee84f2cc09fd2a868374f3c2e571d4aa3e
2251999c <2251999c@student.gla.ac.uk>
2018-03-13 14:45:05 +0000

    Changes made to models.py - still incomplete

--- tatu/models.py
+++ tatu/models.py
@@ -1,21 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#RANGO MODELS.PY FILE BELOW
 #class Category(models.Model):
 #    name = models.CharField(max_length=128, unique=True)
 #    views = models.IntegerField(default=0)
 #    likes = models.IntegerField(default=0)
+#    slug = models.SlugField(unique=True)
+
+#    def save(self, *args, **kwargs):
+#        self.slug = slugify(self.name)
+#        super(Category, self).save(*args, **kwargs)
+
 #    class Meta:
-#        verbose_name_plural = "Categories"
-    
-#    def __str__(self): # For Python 2, use __unicode__ too
+#        verbose_name_plural = 'categories'
+
+#    def __str__(self):
 #        return self.name
-    
+
+
 #class Page(models.Model):
 #    category = models.ForeignKey(Category)
 #    title = models.CharField(max_length=128)
 #    url = models.URLField()
 #    views = models.IntegerField(default=0)
 
-#    def __str__(self): # For Python 2, use __unicode__ too
+#    def __str__(self):
 #        return self.title
+
+#class UserProfile(models.Model):
+#    user = models.OneToOneField(User)
+#    website = models.URLField(blank=True)
+#    picture = models.ImageField(upload_to="profile_images", blank=True)
+#    
+#    def __str__(self):
+#        return self.user.username
+
+# IDs are automatically used according to django docs, so no need to add id fields manually
+
+class Page(models.Model):
+    #user = models.ForeignKey(User)
+    url = models.CharField(max_length=100)
+    
+class UserProfile(models.Model):
+    #user = models.OneToOneField(User)
+    fullname = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=30)
+    picture = models.ImageField(blank=True)
+    workplace = models.CharField(max_length=100, blank=True)
+    phone_no = models.IntegerField(blank=True)
+    website = models.CharField(max_length=100, blank=True)
+    email = models.CharField(max_length=30, blank=True)
+
+    #def __str__(self):
+        #return self.user.username #(?)
+
+class Picture(models.Model):
+    #user = models.ForeignKey(User)
+    tattoo_picture = models.ImageField()
+    tag = models.CharField(max_length=30)
+    rating = models.IntegerField()
+
+class Comment(models.Model):
+    #user = models.ForeignKey(User)
+    #picture = models.ForeignKey(Picture, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+#class FavArtist(models.Model):
+    #user = models.ForeignKey(User)
