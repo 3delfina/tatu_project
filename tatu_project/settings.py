368db9c00f4c2888978da5192a269d165018e0ad
marshymellow <marshymellow@users.noreply.github.com>
2018-03-14 16:20:53 +0000

    just changed index and created some folders and the css page :)

--- tatu_project/settings.py
+++ tatu_project/settings.py
@@ -123,6 +123,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATICFILES_DIRS = [STATIC_DIR]
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR, ]
+
