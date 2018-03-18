969ca3b90e8e3eea43671be04330b137cfdbb55f
SarahCAshworth <seraashworth@gmail.com>
2018-03-18 14:26:14 +0000

    updated requirements.txt

--- tatu/admin.py
+++ tatu/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+from imagekit.admin import AdminThumbnail
 from tatu.models import * 
 
-
-# Register your models here.
 admin.site.register(Page)
 admin.site.register(Comment)
 
 
-admin.site.register(UserProfile)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'admin_thumbnail')
+    admin_thumbnail = AdminThumbnail(image_field='thumbnail')
+
+admin.site.register(UserProfile, PhotoAdmin)
