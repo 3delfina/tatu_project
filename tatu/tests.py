cbf58e2795c3486b58a32fe1aba05e8242a9d6bb
2251999c <2251999c@student.gla.ac.uk>
2018-03-27 16:07:42 +0100

    implemented tests on static files and templates

--- tatu/tests.py
+++ tatu/tests.py
@@ -1,67 +1,78 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.staticfiles import finders
+
+class StaticFileTests(TestCase):
+    # the return value of finders.find(<filename>) is not NONE if static file loads correctly
+    def test_serving_home_background(self):
+        self.assertIsNotNone(finders.find('css/home.png'))
+
+    def test_serving_browse_background(self):
+        self.assertIsNotNone(finders.find('css/browse.png'))
+
+    def test_serving_login_background(self):
+        self.assertIsNotNone(finders.find('css/login.png'))
+
+    def test_serving_register_background(self):
+        self.assertIsNotNone(finders.find('css/register.png'))
+
+    def test_serving_clear_background(self):
+        self.assertIsNotNone(finders.find('css/clear_white.png'))
+
+    def test_serving_other_background(self):
+        self.assertIsNotNone(finders.find('css/other.png'))
 
 class IndexPageTests(TestCase):
-    def test_index_page_loads_okay(self):
-        response = self.client.get(reverse("index"))
-        self.assertEqual(response.status_code, 200)
 
-class TattoosPageTests(TestCase):
+    def test_index_using_template(self):
+        response = self.client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'tatu/index.html')
 
-class CreateAccountPageTests(TestCase):
+class RegisterPageTests(TestCase):
+
+    def test_register_using_template(self):
+        response = self.client.get(reverse('register'))
+        self.assertTemplateUsed(response, 'tatu/register.html')
+
 
 class LoginPageTests(TestCase):
 
+    def test_login_using_template(self):
+        response = self.client.get(reverse('login'))
+        self.assertTemplateUsed(response, 'tatu/login.html')
+
+
+class ContactPageTests(TestCase):
+
+    def test_contact_using_template(self):
+        response = self.client.get(reverse('contact'))
+        self.assertTemplateUsed(response, 'tatu/contact.html')
+
+
+class FAQPageTests(TestCase):
+
+    def test_faq_using_template(self):
+        response = self.client.get(reverse('faq'))
+        self.assertTemplateUsed(response, 'tatu/faq.html')
+
+
+class ArtistsPageTests(TestCase):
+
+    def test_artists_using_template(self):
+        response = self.client.get(reverse('artists'))
+        self.assertTemplateUsed(response, 'tatu/artists.html')
+
+
+"""
 class ProfilePageTests(TestCase):
 
-class RateArtistPageTests(TestCase):
+    def test_profile_using_template(self):
+        response = self.client.get(reverse('profile'))
+        self.assertTemplateUsed(response, 'tatu/profile.html')
+"""
 
-class ModelTests(TestCase):
-    
-    def setUp(self):
-        try:
-            from population_script import populate
-            populate()
-        except ImportError:
-            print("The module 'population_script' does not exist")
-        except NameError:
-            print("The function 'populate()' does not exist, or is not correct")
-        except:
-            print("Something weird went wrong in the populate() function")
+class TattoosPageTests(TestCase):
 
-    def get_page(self, name):
-        from tatu.models import Page
-        try:
-            url = Page.objects.get(url=url)
-        except Page.DoesNotExist:
-            url = None
-        return url
-
-    # once population script, create tests here checking pages have been added
-    # like shown below (just change page urls)
-    def test_(((page url)))_added(self):
-        page = self.get_page("page url")
-        self.assertIsNotNone(page)
-
-    def test_comment_date_not_in_future(self):
-        """
-        the associated date of comments should never be in the future
-        """
-        current_date = date.today()
-        #future_comment = Comment(date=date)
-        self.assertEqual(comment_date < current_date)
-
-    def test_comment_cannot_exceed_length_200(self):
-        text = "a" * 201
-        comment = Comment(text=date)
-
-class ViewTests(TestCase):
-    
-
-class TatuTests(TestCase):
-    def test_
-    
-    def test_1(self):
-        pass
-
-    def test_2(self):
-        pass
+    def test_profile_using_template(self):
+        response = self.client.get(reverse('tattoos'))
+        self.assertTemplateUsed(response, 'tatu/tattoos.html')
