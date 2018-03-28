9842f591b2f2366774213b609a6781b06b7e3f23
2251999c <2251999c@student.gla.ac.uk>
2018-03-28 15:01:32 +0100

    changes to testing

--- tatu/tests.py
+++ tatu/tests.py
@@ -9,6 +9,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.exceptions import ValidationError
 from django.test import Client
 
+
 class StaticFileTests(TestCase):
     # the return value of finders.find(<filepath>) is not NONE if static file loads correctly
     def test_serving_home_background(self):
@@ -30,55 +31,32 @@ class StaticFileTests(TestCase):
         self.assertIsNotNone(finders.find('css/other.png'))
 
 
-class IndexPageTests(TestCase):
+class TemplateTests(TestCase):
 
     def test_index_using_template(self):
         response = self.client.get(reverse('index'))
         self.assertTemplateUsed(response, 'tatu/index.html')
 
-
-class RegisterPageTests(TestCase):
-
     def test_register_using_template(self):
         response = self.client.get(reverse('register'))
         self.assertTemplateUsed(response, 'tatu/register.html')
-
-
-class LoginPageTests(TestCase):
 
     def test_login_using_template(self):
         response = self.client.get(reverse('login'))
         self.assertTemplateUsed(response, 'tatu/login.html')
 
-
-class ContactPageTests(TestCase):
-
     def test_contact_using_template(self):
         response = self.client.get(reverse('contact'))
         self.assertTemplateUsed(response, 'tatu/contact.html')
-
-
-class FAQPageTests(TestCase):
 
     def test_faq_using_template(self):
         response = self.client.get(reverse('faq'))
         self.assertTemplateUsed(response, 'tatu/faq.html')
 
-
-class ArtistsPageTests(TestCase):
-
     def test_artists_using_template(self):
         response = self.client.get(reverse('artists'))
         self.assertTemplateUsed(response, 'tatu/artists.html')
 
-
-"""
-class ProfilePageTests(TestCase):
-
-    def test_profile_using_template(self):
-        response = self.client.get(reverse('profile'))
-        self.assertTemplateUsed(response, 'tatu/profile.html')
-"""
 
 class TattoosPageTests(TestCase):
 
@@ -96,8 +74,8 @@ class ModelTests(TestCase):
         u1 = User.objects.create_user('John', 'lennon@thebeatles.com', 'johnpassword')
         u2 = User.objects.create_user('Ringo', 'starr@thebeatles.com', 'ringopassword')
 
-        p1 = UserProfile.objects.create(user=u1, website='lennon.com')
-        p2 = UserProfile.objects.create(user=u2, website='starr.com')
+        p1 = UserProfile.objects.create(user=u1, website='http://www.lennon.com/')
+        p2 = UserProfile.objects.create(user=u2, website='http://www.starr.com/')
 
         image = SimpleUploadedFile(name='test_image.jpg',
                                    content=open(os.path.join(MEDIA_DIR, 'test', 'test_image.jpg'), 'rb').read(),
@@ -115,7 +93,7 @@ class ModelTests(TestCase):
     def test_profile_user_relationship(self):
         # ensure Ringo's user instance is associated with his UserProfile instance
         try:
-            ringo_profile = UserProfile.objects.get(website='starr.com')
+            ringo_profile = UserProfile.objects.get(website='http://www.starr.com/')
             User.objects.get(userprofile=ringo_profile)
         except:
             self.fail('Profile creation is dysfunctional.')
@@ -153,16 +131,18 @@ class ModelTests(TestCase):
             self.fail('Something a bit weird went wrong in like creation')
 
     def test_profile_workplace_max_length(self):
-        # ensure no profile can have a workplace longer than 100chars
-        # test this by trying to alter John's workplace
-        with self.assertRaises(ValidationError):
-            workplace = 'a'*101
-            print(workplace)
-            john = User.objects.get(username='John')
-            profile = UserProfile.objects.get(user=john)
-            profile.workplace = workplace
-            profile.save()
-
+        try:
+            ringo = User.objects.get(username='Ringo')
+            profile = UserProfile(
+                user=ringo,
+                website='http://www.starr.com/',
+                workplace='a'*101
+            )
+            profile.full_clean()
+            print(profile)
+            self.fail('Character limit on workplace attribute within profile not enforced')
+        except:
+            pass
 
 
 class FormTests(TestCase):
@@ -178,8 +158,8 @@ class FormTests(TestCase):
         except:
             print('Something freaky went wrong yee-ha')
 
-# miscellaneous tests?
-class MiscellaneousTests(TestCase):
+
+class OtherTests(TestCase):
 
     def test_login_works(self):
         # creates user, then attempts to log in using those credentials
@@ -190,35 +170,4 @@ class MiscellaneousTests(TestCase):
         c = Client()
         logged_in = c.login(username='test_user', password='thisismypassword')
         self.assertTrue(logged_in)
-
-"""
-## IS ANY OF THIS NECESSARY? ##
-
-class PopScriptTests(TestCase):
-
-    def setUp(self):
-        try:
-            from population_script import populate
-            populate()
-        except ImportError:
-            print('The population_script module does not exist')
-        except NameError:
-            print('The function populate() either does not exist or is incorrect')
-        except:
-            print('Something funky went wrong in the populate() function')
-
-    usernames = ['xo_g1ve_m3_h0p3_xo', 'jezza32', 'pizzagirl82', 'bronzedidol', 'miss.sporty132',
-                 'solarsparkle', 'tattooking', 'iHeartCookies', 'ToTaLeClipse', 'showstopper']
-"""
-
-
-# POTENTIAL TESTS
-
-"""
-# test_<page>_contains_img (checks the page contains an image)
-response = self.client.get(reverse('<page>'))
-self.assertIn('img', response.content)
-"""
-
-#assertRedirects is a thing
 
