220026989f16566d27865f47b0ee52a37abb851f
2251999c <2251999c@student.gla.ac.uk>
2018-03-28 10:31:08 +0100

    pop script updated to fit with new models.py

--- population_script.py
+++ population_script.py
@@ -1,14 +1,12 @@
-# -*- coding: utf-8 -*-
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tatu_project.settings')
 
 import django
 django.setup()
-from tatu.models import UserProfile, Post, Comment
-from django.contrib.auth.models import User
 import datetime
-from imagekit.models import ProcessedImageField
+from django.contrib.auth.models import User
+from tatu.models import UserProfile, Post, Comment, Like
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 MEDIA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media')
@@ -123,166 +121,167 @@ def populate():
 
 # each set of posts is attributed to a specific user.
 # each user has an associated list of posts they've created.
-# ISSUE: need to work out how to allow for dates in the past - get round auto_now_add
+# ISSUE: remember to sort the dates!
+
     posts = {
         "xo_g1ve_m3_h0p3_xo": [
             {"category": "GM",
              "description": "Geometric nature",
              "date": datetime.date(2018,2,12),
-             "likes": 13,
+             #"likes": 13,
              "filename": "gm1.jpg"},
             {"category": "BW",
              "description": "Blackwork heart",
              "date": datetime.date(2018,2,16),
-             "likes": 17,
+             #"likes": 17,
              "filename": "bw2.jpg"},
         ],
         "jezza32": [
             {"category": "RL",
              "description": "Zeus. Super proud of this one.",
              "date": datetime.date(2018,2,16),
-             "likes": 5,
+             #"likes": 5,
              "filename": "rl1.jpg"},
             {"category": "WC",
              "description": "Galactic diamond",
              "date": datetime.date(2018,2,3),
-             "likes": 41,
+             #"likes": 41,
              "filename": "wc3.jpg"},
         ],
         "pizzagirl82": [
             {"category": "BW",
              "description": "Intricate dragon design",
              "date": datetime.date(2018,2,15),
-             "likes": 2,
+             #"likes": 2,
              "filename": "bw1.jpg"},
             {"category": "JP",
              "description": "Koi tattoo",
              "date": datetime.date(2018,2,5),
-             "likes": 5,
+             #"likes": 5,
              "filename": "jp1.jpg"},
             {"category": "GM",
              "description": "A very geometric mountainside",
              "date": datetime.date(2018,2,7),
-             "likes": 19,
+             #"likes": 19,
              "filename": "gm3.jpg"},
         ],
         "bronzedidol": [
             {"category": "WC",
              "description": "Watercolour birds",
              "date": datetime.date(2018,2,10),
-             "likes": 21,
+             #"likes": 21,
              "filename": "wc1.jpg"},
             {"category": "JP",
              "description": "Dragon tattoo on torso",
              "date": datetime.date(2018,2,11),
-             "likes": 33,
+             #"likes": 33,
              "filename": "jp3.jpg"},
             {"category": "TR",
              "description": "",
              "date": datetime.date(2018,2,14),
-             "likes": 19,
+             #"likes": 19,
              "filename": "tr3.jpg"},
         ],
         "miss.sporty132": [
             {"category": "DW",
              "description": "Veeery intricate dotwork!",
              "date": datetime.date(2018,2,16),
-             "likes": 20,
+             #"likes": 20,
              "filename": "dw1.jpg"},
         ],
         "solarsparkle": [
             {"category": "WC",
              "description": "Leaping fox",
              "date": datetime.date(2018,2,8),
-             "likes": 18,
+             #"likes": 18,
              "filename": "wc2.jpg"},
             {"category": "TD",
              "description": "Spooky",
              "date": datetime.date(2018,2,21),
-             "likes": 16,
+             #"likes": 16,
              "filename": "td1.jpg"},
             {"category": "TD",
              "description": "Every path leads home",
              "date": datetime.date(2018,2,21),
-             "likes": 6,
+             #"likes": 6,
              "filename": "td3.jpg"},
             {"category": "RL",
              "description": "My most realistic animal tattoo to date!",
              "date": datetime.date(2018,2,1),
-             "likes": 4,
+             #"likes": 4,
              "filename": "rl3.jpg"},
             {"category": "LT",
              "description": "beautiful",
              "date": datetime.date(2018,2,2),
-             "likes": 35,
+             #"likes": 35,
              "filename": "lt2.jpg"},
             {"category": "LT",
              "description": "no future",
              "date": datetime.date(2018,2,2),
-             "likes": 34,
+             #"likes": 34,
              "filename": "lt3.jpg"},
         ],
         "tattooking": [
             {"category": "TR",
              "description": "First tribal tattoo",
              "date": datetime.date(2018,2,13),
-             "likes": 5,
+             #"likes": 5,
              "filename": "tr1.jpg"},
             {"category": "TD",
              "description": "Had a whale of a time with this one",
              "date": datetime.date(2018,2,6),
-             "likes": 7,
+             #"likes": 7,
              "filename": "td2.jpg"},
             {"category": "LT",
              "description": "blessed",
              "date": datetime.date(2018,2,2),
-             "likes": 11,
+             #"likes": 11,
              "filename": "lt1.jpg"},
         ],
         "iHeartCookies": [
             {"category": "GM",
              "description": "Minimalist geometric tattoo",
              "date": datetime.date(2018,2,1),
-             "likes": 7,
+             #"likes": 7,
              "filename": "gm2.jpg"},
         ],
         "ToTaLeClipse": [
             {"category": "TR",
              "description": "Tribal",
              "date": datetime.date(2018,2,18),
-             "likes": 32,
+             #"likes": 32,
              "filename": "tr2.jpg"},
             {"category": "RL",
              "description": "Hyper-realistic eye tattoo",
              "date": datetime.date(2018,2,25),
-             "likes": 19,
+             #"likes": 19,
              "filename": "rl2.jpg"},
             {"category": "JP",
              "description": "Japanese castle",
              "date": datetime.date(2018,2,20),
-             "likes": 19,
+             #"likes": 19,
              "filename": "jp2.jpg"},
         ],
         "showstopper": [
             {"category": "TD",
              "description": "One of my works from when I first started out. It had...room for improvement.",
              "date": datetime.date(2018,2,19),
-             "likes": 64,
+             #"likes": 64,
              "filename": "td4.jpg"},
             {"category": "DW",
              "description": "Dotwork spiral",
              "date": datetime.date(2018,2,22),
-             "likes": 47,
+             #"likes": 47,
              "filename": "dw2.jpg"},
             {"category": "DW",
              "description": "Dotwork band",
              "date": datetime.date(2018,2,19),
-             "likes": 38,
+             #"likes": 38,
              "filename": "dw3.jpg"},
             {"category": "BW",
              "description": "Blackwork spiral",
              "date": datetime.date(2018,2,13),
-             "likes": 12,
+             #"likes": 12,
              "filename": "bw3.jpg"},
         ],
     }
@@ -358,15 +357,47 @@ def populate():
          "post_id": 6},
     ]
 
+    likes = [
+        {"liker": "solarsparkle", "post_id": 1},
+        {"liker": "ToTaLeClipse", "post_id": 3},
+        {"liker": "bronzedidol", "post_id": 3},
+        {"liker": "miss.sporty132", "post_id": 4},
+        {"liker": "showstopper", "post_id": 4},
+        {"liker": "iHeartCookies", "post_id": 4},
+        {"liker": "jezza32", "post_id": 6},
+        {"liker": "xo_g1ve_m3_h0p3_xo", "post_id": 6},
+        {"liker": "tattooking", "post_id": 7},
+        {"liker": "pizzagirl82", "post_id": 8},
+        {"liker": "solarsparkle", "post_id": 9},
+        {"liker": "iHeartCookies", "post_id": 10},
+        {"liker": "ToTaLeClipse", "post_id": 12},
+        {"liker": "bronzedidol", "post_id": 12},
+        {"liker": "iHeartCookies", "post_id": 12},
+        {"liker": "pizzagirl82", "post_id": 14},
+        {"liker": "jezza32", "post_id": 20},
+        {"liker": "iHeartCookies", "post_id": 22},
+        {"liker": "solarsparkle", "post_id": 22},
+        {"liker": "solarsparkle", "post_id": 26},
+        {"liker": "pizzagirl82", "post_id": 27},
+    ]
+
     for ix, user_info in enumerate(users):
         user = add_user(user_info)
         username = user.get_username()
         add_user_profile(user, user_profiles[username])
         for post_info in posts[username]:
-            add_post(user, post_info)
+            post = add_post(user, post_info)
 
     for comment in comments:
         add_comment(comment)
+
+    for like in likes:
+        add_like(like)
+
+    # this sets the number of likes for a post = number of like objects registered to the post
+    for post in Post.objects.all():
+        post.likes = Like.objects.filter(post=post).count()
+        post.save()
 
 def add_user(entry):
     user = User.objects.create_user(username=entry["username"],
@@ -404,7 +435,8 @@ def add_post(user, dict):
                                       image=image,
                                       description=dict["description"],
                                       date=dict["date"],
-                                      likes=dict["likes"])[0]
+                                      #likes=dict["likes"])[0]
+                                      )[0]
     post.save()
     return post
 
@@ -420,6 +452,14 @@ def add_comment(info):
     comment.save()
     return comment
 
+def add_like(info):
+    user = User.objects.get(username=info['liker'])
+    post = Post.objects.get(id=info['post_id'])
+    like, aaa = Like.objects.get_or_create(user=user, post=post)
+    like.save()
+    return like
+
+
 if __name__ == '__main__':
-    print("Starting Tatu population script...")
+    print("Running Tatu population script...")
     populate()
